f8f3b57ec0d4151cc3acf4f6ea3cfd7796977314
Pedro Souza <79948864+PedroIsidorio29@users.noreply.github.com>
2021-06-18 22:37:52 -0300

    Delete back directory

--- back/routes/veiculo.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const VeiculoController = require('../controllers/veiculoController');
-const routes = Router();
-
-routes.get('/listar', VeiculoController.getAll);
-
-routes.post('/add', VeiculoController.create);
-
-module.exports = routes;
